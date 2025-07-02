f57d0bc784de01e9b1f554158d7d505b4ec43415
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-07-02 16:51:17 +0530

    changes

---
+++ web/js/script.js
@@ -0,0 +1,10 @@
+let menubtn=document.querySelector(".menubtn");
+let closingbtn=document.querySelector(".closemenu");
+let menu=document.querySelector(".menu");
+
+function showMenu(){
+    menu.style.right="0";
+}
+function hideMenu(){
+     menu.style.right="-350px";
+}
